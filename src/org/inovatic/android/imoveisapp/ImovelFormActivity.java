3de23840a7389296835559a9e36fafeb1fe674bd
carlosgracite <carlosgracite@gmail.com>
2014-05-19 18:58:52 -0300

    Adicionado insert de imóvel no banco de dados.

--- src/org/inovatic/android/imoveisapp/ImovelFormActivity.java
+++ src/org/inovatic/android/imoveisapp/ImovelFormActivity.java
@@ -3,17 +3,21 @@ package org.inovatic.android.imoveisapp;
 import java.util.List;
 
 import org.inovatic.android.imoveisapp.bd.DatabaseHandler;
+import org.inovatic.android.imoveisapp.bd.dao.ImovelDao;
 import org.inovatic.android.imoveisapp.bd.dao.TipoDao;
+import org.inovatic.android.imoveisapp.model.Imovel;
 import org.inovatic.android.imoveisapp.model.Tipo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ImovelFormActivity extends ActionBarActivity {
+public class ImovelFormActivity extends ActionBarActivity 
+	implements View.OnClickListener {
 	
 	private EditText nomeEditText;
 	private EditText descricaoEditText;
@@ -29,6 +33,9 @@ public class ImovelFormActivity extends ActionBarActivity {
 		descricaoEditText = (EditText)findViewById(R.id.editText2);
 		precoEditText = (EditText)findViewById(R.id.editText3);
 		tiposSpinner = (Spinner)findViewById(R.id.spinner1);
+		
+		View saveButton = findViewById(R.id.button1);
+		saveButton.setOnClickListener(this);
 		
 		populateSpinner();
 	}
@@ -54,6 +61,47 @@ public class ImovelFormActivity extends ActionBarActivity {
 		dbHandler.close();
 		
 		return result;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.button1:
+			save();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void save() {
+		Imovel imovel = buildImovel();
+		
+		DatabaseHandler dbHandler = new DatabaseHandler(this);
+		ImovelDao dao = new ImovelDao(dbHandler);
+		long id = dao.insert(imovel);
+		dbHandler.close();
+		
+		if (id > 0) {
+			Toast.makeText(this, "O imóvel foi inserido com sucesso.", 
+					Toast.LENGTH_SHORT).show();
+			finish();
+		} else {
+			Toast.makeText(this, "Erro ao inserir imóvel.", 
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private Imovel buildImovel() {
+		Imovel imovel = new Imovel();
+		
+		imovel.nome = nomeEditText.getText().toString();
+		imovel.descricao = descricaoEditText.getText().toString();
+		imovel.preco = Integer.parseInt(precoEditText.getText().toString());
+		imovel.tipo = (Tipo)tiposSpinner.getSelectedItem();
+		
+		return imovel;
 	}
 
 }
