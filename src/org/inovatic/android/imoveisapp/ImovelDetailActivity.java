048613976ee5a62e4fde068b3854f2aa05db40a3
Carlos Eduardo Gracite do Amaral <carlosgracite@gmail.com>
2014-05-30 12:58:13 -0300

    Adicionada possibilidade de ver o trajeto da localização atual do usuário até o imóvel desejado.

--- src/org/inovatic/android/imoveisapp/ImovelDetailActivity.java
+++ src/org/inovatic/android/imoveisapp/ImovelDetailActivity.java
@@ -5,8 +5,10 @@ import org.inovatic.android.imoveisapp.bd.dao.ImovelDao;
 import org.inovatic.android.imoveisapp.model.Imovel;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class ImovelDetailActivity extends ActionBarActivity {
@@ -17,6 +19,8 @@ public class ImovelDetailActivity extends ActionBarActivity {
 	private TextView descricaoTextView;
 	private TextView tipoTextView;
 	private TextView precoTextView;
+	
+	private Imovel imovel;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,25 @@ public class ImovelDetailActivity extends ActionBarActivity {
 		if (i.hasExtra(EXTRA_IMOVEL_ID)) {
 			populateInterface(i.getLongExtra(EXTRA_IMOVEL_ID, 0));
 		}
+		
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startMaps(imovel.latitude, imovel.longitude);
+			}
+		});
+	}
+	
+	private void startMaps(float latitude, float longitude) {
+		Intent intent = new Intent(Intent.ACTION_VIEW, 
+				Uri.parse("http://maps.google.com/maps?daddr="
+						+ latitude + ","
+						+ longitude));
+		startActivity(intent);
 	}
 
 	private void populateInterface(long id) {
-		Imovel imovel = queryImovelFromDatabase(id);
+		imovel = queryImovelFromDatabase(id);
 		
 		nomeTextView.setText(imovel.nome);
 		descricaoTextView.setText(imovel.descricao);
